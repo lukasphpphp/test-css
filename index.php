ed642233ce5fbfc4ff50863d366bd25c7bd70991
Lukas Nevedel <Lukas Nevedel@DESKTOP-PT8BK2M>
2020-04-13 22:15:25 +0200

    commit 13.4.

--- index.php
+++ index.php
@@ -1,18 +1,58 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="cs">
   <head>
    <title>Sidney Crosby</title>
    <link href="css.css" rel="stylesheet">
   </head>
- <body>
-     <a id="sidney">
-       <h1>Sidney Crosby</h1>
-     </a>
-     <img src="Crosby.jpg" alt="crosby">>
+ 
+  <body>
+     <header>
+      <div class="listacela">       
+         <ul id='lista' >              
+             <li> <a href="#prihlaseni"> Přihlášení </a> </li>
+             <li> <a href="#prihlaseni"> Něco jinýho </a> </li>
+         </ul>
+      <div>
+      <a id="sidney">
+         <h1>Sidney Crosby</h1>
+      </a>
+      </div>
+          <br>
+     </div>
+     </header>
+      
+      <div class="nahoru">
+          <div class="nahoru2">
+              
+                  <a href="#sidney"><h4 id="mensi">nahoru</h4> </a>
+              
+          </div>
+          
+      </div>
+      
+      
+      
+     <hr>
+      <img src="Crosby.jpg" alt="crosby" id="fotka">
+     <hr>
      
+     <h3>Obsah</h3>
+     <ul>
+         <li> <a href="#kdo to je"> Kdo to je  </li> </a>
+         <li> <a href="#historie"> Historie </li></a>
+         <li> <a href="#Odkazy dále"> Odkazy dále </li></a>
+         <ol>
+             <li>NHL Kariéra  </li>
+             <li> Více informací  </li>
+         </ol>         
+         <li> <a href="#prihlaseni"> Přihlášení </li></a>
+         <li> <a href="#tabulka"> tabulka</li> </a>   
+     </ul>
+     <br>
+     <hr>
      
-     <div class="kdo to je">
-         <div class="nadpis">
+     <div>
+         <div id='kdo to je'>
           <h3> Kdo to je?</h3>
          </div>
       <div>
@@ -20,52 +60,99 @@
      </div>
      (* 7. srpna 1987, Halifax, Nové Skotsko, Kanada) je kanadský hokejový útočník. Hraje za klub Pittsburgh Penguins v NHL a na mezinárodní scéně reprezentuje Kanadu.
      <p>
-     <br>   
-     Již před vstupem do NHL byl považován za jeden z největších talentů historie. Ve své úvodní sezóně 05/06 získal v kanadském bodování 102 bodů a stal se nejmladším hráčem v historii NHL, kterému se podařilo překonat stobodovou hranici. V roce 2007 byl jmenován kapitánem Pittsburghu a stal se nejmladším kapitánem v historii NHL. V roce 2009 jako nejmladší kapitán v historii dovedl svůj tým k vítězství ve Stanley Cupu. Na ZOH 2010 získal s kanadskou reprezentací zlatou olympijskou medaili, když v prodloužení rozhodl finálový zápas proti USA. Na ZOH 2014 získal také zlatou medaili. Po zisku zlaté medaile z mistrovství světa v roce 2015 v Praze se stal členem prestižního Triple Gold Clubu.
-     </p>
+     
+     <br>
+     <hr>
+     
+     <div>
+         <div id="historie">
+           <h3>Historie</h3>
+         </div>
+         <div>
+             Již před vstupem do NHL byl považován za jeden z největších talentů historie. Ve své úvodní sezóně 05/06 získal v kanadském bodování 102 bodů a stal se nejmladším hráčem v historii NHL, kterému se podařilo překonat stobodovou hranici. V roce 2007 byl jmenován kapitánem Pittsburghu a stal se nejmladším kapitánem v historii NHL. V roce 2009 jako nejmladší kapitán v historii dovedl svůj tým k vítězství ve Stanley Cupu. Na ZOH 2010 získal s kanadskou reprezentací zlatou olympijskou medaili, když v prodloužení rozhodl finálový zápas proti USA. Na ZOH 2014 získal také zlatou medaili. Po zisku zlaté medaile z mistrovství světa v roce 2015 v Praze se stal členem prestižního Triple Gold Clubu.
+        </div>
+      </div>
      </div> 
      
      
      
+     
+     
+     
      <br>
-    <center>
-     <b> NHL Kariéra </b>
-    <br> <a href="index2.php">odkaz zde</a> 
+     
+    <div>
+      <center>
+     <b id="Odkazy dále"> NHL Kariéra </b>
+    <br> 
+    <a href="index2.php" class="link">odkaz zde</a> 
     <br>
     <br>
      <b> Více informací</b>
     <br>
-    <a href="https://cs.wikipedia.org/wiki/Sidney_Crosby">Klikni zde</a> 
+    <a href="https://cs.wikipedia.org/wiki/Sidney_Crosby" class="link">Klikni zde</a> 
     </center>
+     </div>
+     
+     
+     
     <br>
+    <hr>
+    
+    <div id="prihlaseni">
+         <br>
+               <center>
+                   <i>Pokud chcete dostávat aktuální informace prosím přihlaste se </i>
+               </center>
+         <div class="hesla" >
+             <center>
+                  <table class="heslatabulka" span style="margin-top: 20px;">           
+                      <tbody>
+                         <tr>
+                             <td><input type = text> Jméno <br>
+                             <input type = password> Heslo<br>
+                             <select name = pohlaví> <br>
+                                 <option value = 1> Muž
+                                 <option value = 2> Žena
+                            </select> Vyberte pohlaví <br>
+                            <input type="checkbox"> Pamatovat si mě<br>
+                            <div id="tlacitko">
+                              <button type="submit" >Přihlásit</button>
+                            </div></td>
+                         </tr>            
+                     </tbody>
+                 </table>
+            </center>
+         </div>
     <br>
-     <i>Pokud chcete dostávat aktuální informace prosím přihlaste se </i>
-     <br>
-     <input type = text> jméno
-     <br>
-     <input type = password> heslo
-     <br>
-     <select name = pohlaví>
-         <option value = 1> Muž
-         <option value = 2> Žena
-     </select>
-     <br>
+    </div>     
+    
+    <hr> 
+    
+    
      <br>
      <br>
      <span style="color: yellow;">Nyní tabulka</span>
-     
-     <table>
-     <tr> <th colspan="2">1.1</th> <td>1.3</td> <td>1.4</td> <td>1.5</td> <td>1.6</td> <td>1.7</td> <td>1.8</td> <td>1.9</td> <td>1.10</td></tr>
-     <tr> <th rowspan="2">2.1</th> <td>2.2</td> <td>2.3</td> <td>2.4</td> <td>2.5</td> <td>2.6</td> <td>2.7</td> <td>2.8</td> <td>2.9</td> <td>2.10</td></tr>
-     <tr> <td>3.2</td> <td>3.3</td> <td>3.4</td> <td>3.5</td> <td>3.6</td> <td>3.7</td> <td>3.8</td> <td>3.9</td> <td>3.10</td></tr>
-     <tr> <th>4.1</th> <td>4.2</td> <td>4.3</td> <td>4.4</td> <td>4.5</td> <td>4.6</td> <td>4.7</td> <td>4.8</td> <td>4.9</td> <td>4.10</td></tr>
-     <tr> <th>5.1</th> <td>5.2</td> <td>5.3</td> <td>5.4</td> <td>5.5</td> <td>5.6</td> <td>5.7</td> <td>5.8</td> <td>5.9</td> <td>5.10</td></tr>
-     <tr> <th>6.1</th> <td>6.2</td> <td>6.3</td> <td>6.4</td> <td>6.5</td> <td>6.6</td> <td>6.7</td> <td>6.8</td> <td>6.9</td> <td>6.10</td></tr>
-     <tr> <th>7.1</th> <td>7.2</td> <td>7.3</td> <td>7.4</td> <td>7.5</td> <td>7.6</td> <td>7.7</td> <td>7.8</td> <td>7.9</td> <td>7.10</td></tr>
-     <tr> <th>8.1</th> <td>8.2</td> <td>8.3</td> <td>8.4</td> <td>8.5</td> <td>8.6</td> <td>8.7</td> <td>8.8</td> <td>8.9</td> <td>8.10</td></tr> 
-     <tr> <th>9.1</th> <td>9.2</td> <td>9.3</td> <td>9.4</td> <td>9.5</td> <td>9.6</td> <td>9.7</td> <td>9.8</td> <td>9.9</td> <td>9.10</td></tr>
-     <tr> <th>10.1</th> <td>10.2</td> <td>10.3</td> <td>10.4</td> <td>10.5</td> <td>10.6</td> <td>10.7</td> <td>10.8</td> <td>10.9</td> <td>10.10</td></tr>
+     <div id="tabulka">
+      <table>
+       <tr> <th colspan="2">1.1</th> <td>1.3</td> <td>1.4</td> <td>1.5</td> <td>1.6</td> <td>1.7</td> <td>1.8</td> <td>1.9</td> <td>1.10</td></tr>
+       <tr> <th rowspan="2">2.1</th> <td>2.2</td> <td>2.3</td> <td>2.4</td> <td>2.5</td> <td>2.6</td> <td>2.7</td> <td>2.8</td> <td>2.9</td> <td>2.10</td></tr>
+       <tr> <td>3.2</td> <td>3.3</td> <td>3.4</td> <td>3.5</td> <td>3.6</td> <td>3.7</td> <td>3.8</td> <td>3.9</td> <td>3.10</td></tr>
+       <tr> <th>4.1</th> <td>4.2</td> <td>4.3</td> <td>4.4</td> <td>4.5</td> <td>4.6</td> <td>4.7</td> <td>4.8</td> <td>4.9</td> <td>4.10</td></tr>
+       <tr> <th>5.1</th> <td>5.2</td> <td>5.3</td> <td>5.4</td> <td>5.5</td> <td>5.6</td> <td>5.7</td> <td>5.8</td> <td>5.9</td> <td>5.10</td></tr>
+       <tr> <th>6.1</th> <td>6.2</td> <td>6.3</td> <td>6.4</td> <td>6.5</td> <td>6.6</td> <td>6.7</td> <td>6.8</td> <td>6.9</td> <td>6.10</td></tr>
+       <tr> <th>7.1</th> <td>7.2</td> <td>7.3</td> <td>7.4</td> <td>7.5</td> <td>7.6</td> <td>7.7</td> <td>7.8</td> <td>7.9</td> <td>7.10</td></tr>
+       <tr> <th>8.1</th> <td>8.2</td> <td>8.3</td> <td>8.4</td> <td>8.5</td> <td>8.6</td> <td>8.7</td> <td>8.8</td> <td>8.9</td> <td>8.10</td></tr> 
+       <tr> <th>10.1</th> <td>10.2</td> <td>10.3</td> <td>10.4</td> <td>10.5</td> <td>10.6</td> <td>10.7</td> <td>10.8</td> <td>10.9</td> <td>10.10</td></tr>
      </table>
+   </div> 
     
+     
+     
+                       <table class="heslatabulka" span= "padding-top": 25px;>
+
+     
+     
+     
  </body>
 </html>
