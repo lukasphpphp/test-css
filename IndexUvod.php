efe4919799da4105f23c95f5722698405e6f2ada
Lukas Nevedel <Lukas Nevedel@DESKTOP-PT8BK2M>
2020-04-23 13:20:36 +0200

    commit 34.4. php uvod do prg HOTOVO

--- IndexUvod.php
+++ IndexUvod.php
@@ -1,2 +1,62 @@
 <?php
-echo 
+$PromennaBoleen = true;                   //ukol c. 4.
+    echo $PromennaBoleen;
+    echo '<br>';
+    print $PromennaBoleen;
+    echo '<p>';
+    
+ $PromennaString = 'neco';
+    echo $PromennaString;
+    echo '<br>';
+    print $PromennaString;
+    echo '<p>';
+    
+ $PromennaFloat = 20.1; 
+   echo $PromennaFloat;
+   echo '<br>';
+   print $PromennaFloat;
+   echo '<p>';
+   
+ $PromennaInteger = 10;
+   echo $PromennaInteger;
+   echo '<br>';
+   print $PromennaInteger;
+   echo '<p>';
+   
+
+  $PromennaSpojeni = $PromennaInteger . $PromennaFloat . 'czk';  //ukol c. 5.
+   echo $PromennaSpojeni;
+   echo '<br>';
+   
+   
+   echo '<img src="Crosby.jpg" alt="crosby">';    //ukol c. 6.
+   echo '<br>';
+   echo '<br>';
+   
+   
+   echo '\\';  //ukol c. 7.
+
+   
+  $a = 10;          //ukol c. 8.
+  $b = 20;
+  
+  var_dump($a+$b);
+  var_dump($a-$b);
+  var_dump($a/$b);
+  var_dump($a*$b);
+  
+  var_dump ($a++);    //tady tech 7 radku si dela nejak co chce nechapu 
+  var_dump ($a--);
+  var_dump ($a += $b);
+  var_dump ($a -= $b);
+  var_dump ($a *= $b);
+  var_dump ($a /= $b);
+  
+  var_dump ($a==$b);
+  var_dump ($a != $b);
+  var_dump ($a>$b);
+  var_dump ($a<$b);
+  
+  var_dump ($a && $b);
+  var_dump (!$a);
+   
