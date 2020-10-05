a390a9f7dc260c2128d70f488ee4d685bcb23ab4
Lukas Nevedel <Lukas Nevedel@DESKTOP-PT8BK2M>
2020-10-05 23:19:25 +0200

    Nedodelene oop

---
+++ oop.php
@@ -0,0 +1,27 @@
+<?php
+
+class user {
+    public $a = 10;
+    public $b = 525;
+    public $c = 65;
+    public $d = 354;
+    public $e = 'cau';
+            
+    protected $f;
+    protected $g;
+    protected $h;
+            
+    private $i ;
+    private $j ;
+    
+    public function __construct() {}
+    
+    }
+            
+
+
+
+public function getFoo
+return $this->foo;
+ 
+ var_dump user;
